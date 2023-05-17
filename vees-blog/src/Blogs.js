4ebcaa48426e51ff25f929f1f5935072949c4735
Ovy Evbodi <96619760+OvyEvbodi@users.noreply.github.com>
2023-05-17 23:41:26 +0000

    Feat: add json database

--- vees-blog/src/Blogs.js
+++ vees-blog/src/Blogs.js
@@ -1,7 +1,4 @@
-import { useState } from 'react';
-
-
-const Blogs = ({blogs, title}) => {
+const Blogs = ({blogs, title, handleDelete}) => {
 
     // render preview for each blog
     return (
@@ -11,6 +8,7 @@ const Blogs = ({blogs, title}) => {
                 <div className="blog_title my-3 p-1 border-4 border-danger-subtle border-start rounded shadow" key={ blog.id }>
                     <h3 className="text-body-secondary display-6 ps-3">{ blog.title }</h3>
                     <p className="ps-3">written by: <span className='text-danger'>{ blog.author }</span></p>
+                    <button type="button" onClick={ () => handleDelete(blog.id) } className="btn ms-3 px-3 btn-sm btn-outline-danger">Delete</button>
                 </div>
             ))
             }
--- vees-blog/src/Home.js
+++ vees-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Blogs from './Blogs';
 
 const Home = () => {
@@ -12,16 +12,21 @@ const Home = () => {
     {id: 1, title: 'Many chances', author: 'Ovy Evbodi', desc:'Giving life another shot every day'},
     {id: 2, title: 'A forgiving heart', author: 'Smurfy Smurf', desc: "I don't wanna poo"},
     {id: 3, title: "Get out now", author: "Shims boo", desc: "Feeling like staying out"},
-    {id: 4, title: "Four for four", author: "Not sure", desc: "We're tired we're not tired"}
+    {id: 4, title: "Four for four", author: "Not sure", desc: "We're tired we're not tired"},
+    {id:5, title: "The future finally came", author: "Ovy Evbodi", desc: "It's always a breath of fresh air when we evenntually get it right"}
 ]);
 
-
-  const handleClick = () => setName('Vee baby!!.....')
+  useEffect(() => console.log('killing this!!'), [blogs])
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id)
+    setBlogs(newBlogs)
+  };
     return (
     <div>
         <h1>Vee's blog</h1>
         <p>...for real this time, lol.</p>
-        <Blogs blogs={blogs} title="All blogs"/>
+        <Blogs blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
+        <Blogs blogs={blogs.filter((blog) => blog.author === 'Ovy Evbodi')} title="Ovy's blogs" handleDelete={handleDelete}/>
       </div>
     )
 };
