ee895438fa7a1fecaab77a1368c1bc9f60745cc9
Ovy Evbodi <96619760+OvyEvbodi@users.noreply.github.com>
2023-05-20 20:51:31 +0000

    Link js script

--- typescript_field/index.js
+++ typescript_field/index.js
@@ -1,1 +1,8 @@
-save
+var spendingType = document.getElementById('finance');
+var sourceDest = document.getElementById('finance_name');
+var amount = document.getElementById('amount');
+var form = document.querySelector('form');
+var loggit = function () {
+    console.log(spendingType.value, sourceDest.value, amount.value);
+};
+form.addEventListener('submit', loggit);
